61be740a235132df2962a7dfe0b020cca8db71a1
Otto Programmer <otmankhess@gmail.com>
2023-09-10 16:28:19 -0400

    first commit

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-      return new Response(`Bun!`);
-    },
-  });
-  
-  console.log(`Listening on http://localhost:${server.port} ...`);
+import Bao from "baojs";
+
+const app = new Bao();
+const port = parseInt(process.env.PORT || "8080");
+
+app.get("/", (ctx) => {
+  return ctx.sendText("Hello world from Bao.js running on Railway!");
+});
+
+const server = app.listen({ port: port });
+console.log(`Server listening on ${server.hostname}:${port}`);
